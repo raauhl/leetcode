807acfcd3a650a9fa618b28e238a52d784ff0cd6
rahulchauhan <8800556229r@gmail.com>
2023-02-11 18:14:18 +0530

    delete code

--- palindromeLinkedList.py
+++
@@ -1,9 +0,0 @@
-def isPalindrome(head):
-
-    info = []
-    while head:
-        info.append(str(head.val))
-        head = head.next
-    
-    info = "".join(info)
-    return True if info == info[::-1] else False
