19806f2b9e9a688f5ab85602ab4c839d7e11417a
RAHUL <rahul25rocker@gmail.com>
2024-01-11 12:33:43 +0530

    Add files via upload

---
+++ maximum-difference-between-node-and-ancestor.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
+                
+        def f(node):
+                        
+            if node.left is None and node.right is None:
+                return (node.val, node.val)
+            
+            if node.left is not None and node.right is not None:
+                
+                miniLeft, maxxLeft = f(node.left)
+                miniRight, maxxRight = f(node.right)
+                
+                mini = min(miniLeft, miniRight, node.val)
+                maxx = max(maxxLeft, maxxRight, node.val)
+                self.v = max(self.v, abs(node.val-mini), abs(node.val-maxx))
+                
+                return (mini, maxx)
+                
+            
+            if node.left is not None:
+                mini, maxx = f(node.left)
+                self.v = max(self.v, abs(node.val-mini), abs(node.val-maxx))
+                mini = min(mini, node.val)
+                maxx = max(maxx, node.val)
+                return (mini, maxx)
+            
+            if node.right is not None:
+                mini, maxx = f(node.right)
+                self.v = max(self.v, abs(node.val-mini), abs(node.val-maxx))
+                mini = min(mini, node.val)
+                maxx = max(maxx, node.val)
+                return (mini, maxx)
+                
+        self.v = 0
+        f(root)
+        return self.v
+    
